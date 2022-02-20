186690c69a759c89e36905230be5919b22ed6010
Osama4488 <syed.osama.ali.96@gmail.com>
2022-02-20 15:47:15 +0500

    connect to mongo db and data added

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blogs");
+const { result } = require("lodash");
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://osama:osama123@nodetuts.6rrgn.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 // register view engine
 
 app.set("view engine", "ejs");
 
 // listen for requests
 
-app.listen(3000);
+// app.listen(3000);
 
 // middleware & static files
 
@@ -24,17 +34,69 @@ app.use(express.static("public"));
 app.use(morgan("dev"));
 // end
 
+// mongoose and mongo sandbox routes
+
+// app.get("/add-blog", (req, res) => {
+//   const blog = new Blog({
+//     title: "new blog 2",
+//     snippet: "about my new blog",
+//     body: "more about my new blog",
+//   });
+//   blog
+//     .save()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// app.get("/all-blogs", (req, res) => {
+//   Blog.find()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// app.get("/single-blog", (req, res) => {
+//   Blog.findById("621216e09ffe2dbe6a0bf955")
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+//routes
 app.get("/", (req, res) => {
-  const blogs = [
-    { title: "ansd", snippets: "asdnaodsnoansd" },
-    { title: "ansd", snippets: "asdnaodsnoansd" },
-    { title: "ansd", snippets: "asdnaodsnoansd" },
-  ];
-  res.render("index", { title: "Home", blogs });
+  // const blogs = [
+  //   { title: "ansd", snippets: "asdnaodsnoansd" },
+  //   { title: "ansd", snippets: "asdnaodsnoansd" },
+  //   { title: "ansd", snippets: "asdnaodsnoansd" },
+  // ];
+  // res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+// blog routes
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blogs/create", (req, res) => {
